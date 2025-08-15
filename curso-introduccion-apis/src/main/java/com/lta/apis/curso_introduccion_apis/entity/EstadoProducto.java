6c7f7a5c23ea5c8c1b8ee39004d2aa2c57a5ab18
pabloc <pablocarrillopunina@gmail.com>
2025-08-15 19:08:29 +0200

    feat: configuracion y conexion a una base de datos

---
+++ curso-introduccion-apis/src/main/java/com/lta/apis/curso_introduccion_apis/entity/EstadoProducto.java
@@ -0,0 +1,5 @@
+package com.lta.apis.curso_introduccion_apis.entity;
+
+public enum EstadoProducto {
+    DISPONIBLE, NO_DISPONIBLE
+}
---
+++ curso-introduccion-apis/src/main/java/com/lta/apis/curso_introduccion_apis/entity/Producto.java
@@ -0,0 +1,33 @@
+package com.lta.apis.curso_introduccion_apis.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Producto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_producto")
+    private Long idProducto;
+    @Column(name = "nombreProducto", nullable = false, length = 100)
+    private String nombreProducto;
+    @Column(name = "descripcion")
+    private String descripcion;
+
+    @Column(name = "precio" , nullable = false)
+    private Double precio;
+
+    @Column(name = "cantidad", nullable = false)
+    private int cantidad;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "estado", nullable = false)
+    private EstadoProducto estadoProducto;
+}
